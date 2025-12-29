7f02fe7411f188bc0885f4250f562aa29b72a82d
Cristian <liccristian147@gmail.com>
2025-12-29 03:22:53 -0300

    update cloudinary.ts

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -4,11 +4,11 @@ import { FileUploadController } from "./file-upload.controller";
 import { FileUploadRepository } from "./file-upload.repository";
 import { FileUploadService } from "./file-upload.service";
 import { Product } from "../product/entities/product.entity";
-import { CloudianryConfig } from "../config/cloudinary";
+import { CloudinaryConfig } from "../config/cloudinary";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
   controllers: [FileUploadController],
-  providers: [CloudianryConfig, FileUploadRepository, FileUploadService],
+  providers: [CloudinaryConfig, FileUploadRepository, FileUploadService],
 })
 export class fileuploadModule {}
