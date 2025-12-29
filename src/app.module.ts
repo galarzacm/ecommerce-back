03bb8ab7605b02c5c26a647a460f1569d78e30f2
Cristian <liccristian147@gmail.com>
2025-12-29 02:02:24 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,12 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { postgresDataSourceConfig } from "./config/data-source";
-import { ConfigModule, ConfigService } from "@nestjs/config";
+import { ConfigModule } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { UserModule } from "./user/user.module";
 import { CategoryModule } from "./category/category.module";
 import { ProductModule } from "./product/product.module";
-import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { SeedModule } from "./seeds/seed.module";
 import { OrderModule } from "./order/order.module";
-import { Order } from "./order/entities/order.entity";
-import { JwtModule } from "@nestjs/jwt";
 import { AuthModule } from "./auth/auth.module";
 import { fileuploadModule } from "./file-upload/file-upload.module";
 
@@ -18,23 +14,16 @@ import { fileuploadModule } from "./file-upload/file-upload.module";
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: "./config/data-source.ts",
-      load: [postgresDataSourceConfig],
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: async (
-        configService: ConfigService,
-      ): Promise<TypeOrmModuleOptions> => {
-        const config =
-          await configService.get<TypeOrmModuleOptions>("postgres");
 
-        if (!config) {
-          throw new Error('Missing TypeORM config under key "postgres"');
-        }
-        return config;
-      },
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+      autoLoadEntities: true,
+      synchronize: false,
     }),
+
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
@@ -46,11 +35,8 @@ import { fileuploadModule } from "./file-upload/file-upload.module";
     ProductModule,
     SeedModule,
     OrderModule,
-    Order,
     AuthModule,
     fileuploadModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
